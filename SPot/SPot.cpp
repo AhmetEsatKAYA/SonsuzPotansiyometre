742fe5d9fb7b6fb75bc7bb72e871405392bef402
AhmetEsatKAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2023-12-25 12:35:13 +0300

    Update SPot.cpp

--- SPot/SPot.cpp
+++ SPot/SPot.cpp
@@ -1,1 +1,29 @@
+#include "Arduino.h"
+#include "SPot.h" 
 
+SPot::SPot(int pin)
+{
+  _pin = pin;
+}
+
+void SPot::begin()
+{
+  pinMode(_pin, INPUT);
+  int degeri;
+}
+
+void SPot::deger()
+{
+  degeri = analogRead(_pin);
+  return degeri;
+}
+
+void SPot::tur()
+{
+  
+}
+
+void SPot::aci()
+{
+  
+}
