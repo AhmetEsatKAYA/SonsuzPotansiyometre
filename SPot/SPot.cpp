3827f74e459768c5c7a7b5bfe6f48f8f0946f919
AhmetEsatKAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2023-12-28 16:28:41 +0300

    Update SPot.cpp

--- SPot/SPot.cpp
+++ SPot/SPot.cpp
@@ -1,35 +1,43 @@
 #include "Arduino.h"
-#include "SPot.h" 
+#include "SPot.h"
 
 SPot::SPot(int pin)
 {
-  _pin = pin;
+  _pin = pin; 
+  _tur = 0; 
+  _onceki = 0;
 }
 
 void SPot::begin()
 {
-  pinMode(_pin, INPUT);
-  int degeri;
-  int acid;
+  pinMode(_pin, INPUT); 
+  Serial.begin(9600);
 }
 
-void SPot::deger()
+int SPot::deger() 
 {
-  degeri = analogRead(_pin);
-  return degeri;
+  int deger = analogRead(_pin);
+  deger = deger / 10; 
+  return deger; 
 }
 
-void SPot::tur()
+int SPot::tur() 
 {
-  
-}
-
-void SPot::aci()
-{
-  if (deger() == 0) {
-    acid = 0;
-  } else {
-    acid = map(deger(), 0, 1023, 0, 270);
+  int deger = analogRead(_pin); 
+  if (deger == 0 || deger == 1023)
+  {
+    if (deger != _onceki)
+    {
+      _tur++;
+      _onceki = deger;
+    }
   }
-  return acid;
+  return _tur; 
+}
+
+int SPot::aci() 
+{
+  int deger = analogRead(_pin); 
+  int aci = map(deger, 0, 1023, 0, 360); 
+  return aci;
 }
