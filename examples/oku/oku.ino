6c5e6139aa5aaffd578255e2b79a246c9b27cd08
AhmetEsatKAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2023-12-28 16:45:59 +0300

    main

--- examples/oku/oku.ino
+++ examples/oku/oku.ino
@@ -1,25 +1,25 @@
-#include "SPot.h" //kütüphaneyi dahil etmek için
+#include "SPot.h"
 
-SPot pot(A0); //A0 pinine bağlı bir potansiyometre nesnesi oluşturmak için
+SPot pot(A0); 
 
 void setup()
 {
-  pot.begin(); //potansiyometreyi başlatmak için
+  pot.begin(); 
 }
 
 void loop()
 {
-  int deger = pot.deger(); //potansiyometreden gelen değeri almak için
-  int tur = pot.tur(); //potansiyometrenin kaç tur döndüğünü almak için
-  int aci = pot.aci(); //potansiyometrenin değerini açıya dönüştürmek için
+  int deger = pot.deger(); 
+  int tur = pot.tur(); 
+  int aci = pot.aci();
 
-  Serial.print("Deger: "); //seri monitöre değeri yazmak için
+  Serial.print("Deger: ");
   Serial.println(deger);
-  Serial.print("Tur: "); //seri monitöre tur sayısını yazmak için
+  Serial.print("Tur: "); 
   Serial.println(tur);
-  Serial.print("Aci: "); //seri monitöre açıyı yazmak için
+  Serial.print("Aci: ");
   Serial.println(aci);
-  Serial.println(); //boş bir satır yazmak için
+  Serial.println();
 
-  delay(100); //100 ms beklemek için
+  delay(100); 
 }
